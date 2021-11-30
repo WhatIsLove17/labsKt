9f5528784f326f50c2c9905035f4a0af4bdeeab0
WhatIsLove <WhatIsLoveff@gmail.com>
2021-11-30 03:54:28 +0300

    function wordTransfer was created

--- src/lab1/program/text_editor/alignment.kt
+++ src/lab1/program/text_editor/alignment.kt
@@ -24,6 +24,35 @@ private fun toOneLine(text : ArrayList<String>)
     }
 }
 
+private fun wordTransfer(width : Int, text : ArrayList<String>)
+{
+    var i = 0
+    while(i < text.size)
+    {
+        while(text[i].length > width)
+        {
+            // adding new line if this is last
+            if (i == text.size-1) text.add("")
+
+            if (text[i].lastIndexOf(' ') != -1) {
+            // transfer one word if there is space
+                text[i + 1] = text[i].substringAfterLast(' ') +
+                        if (text[i+1].length != 0) {" "} else {""} + text[i + 1]
+                text[i] = text[i].substringBeforeLast(' ')
+            }
+            else
+            {
+            // transfer few symbols if there isn't space
+                text[i + 1] = text[i].substring(width -
+                        if (text[i].length - width == 1 && text[i].last() in arrayOf('!', ',', '.', ':', '-', ';', '?'))
+                        {1} else {0}) + " " + text[i + 1]
+                text[i] = text[i].substring(0, width)
+            }
+        }
+        i++
+    }
+}
+
 enum class Alignment
 {
     LEFT,
