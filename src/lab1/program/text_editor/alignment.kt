8f90c4bdbb13b7c539ffeb326e919bd06598f4ec
Veta <59028485+Elizaveta-git@users.noreply.github.com>
2021-11-30 03:34:30 +0300

    alignText was created

--- src/lab1/program/text_editor/alignment.kt
+++ src/lab1/program/text_editor/alignment.kt
@@ -1,2 +1,24 @@
 package lab1.program.text_editor
 
+fun alignText(alignment: Alignment, width: Int, text : ArrayList<String>)
+{
+    toOneLine(text)
+    spaceDeleter(text)
+    wordTransfer(width, text)
+    when(alignment)
+    {
+        Alignment.JUSTIFY -> alignToWidth(width, text)
+        Alignment.CENTER -> centerAlignment(width, text)
+        Alignment.LEFT -> leftAlignment(width, text)
+        Alignment.RIGHT -> rightAlignment(width, text)
+        else -> return
+    }
+}
+
+enum class Alignment
+{
+    LEFT,
+    RIGHT,
+    CENTER,
+    JUSTIFY
+}
