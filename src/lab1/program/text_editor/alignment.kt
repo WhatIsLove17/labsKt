910286b21e85772846633ecefeb8622b1a6f123e
WhatIsLove <WhatIsLoveff@gmail.com>
2021-12-10 23:15:46 +0300

    comments have been corrected

--- src/lab1/program/text_editor/alignment.kt
+++ src/lab1/program/text_editor/alignment.kt
@@ -1,26 +1,21 @@
 package lab1.program.text_editor
 
-fun alignText(alignment: Alignment, width: Int, text: ArrayList<String>) {
-    toOneLine(text)
-    spaceDeleter(text)
-    wordTransfer(width, text)
+fun alignText(alignment: Alignment = Alignment.JUSTIFY, width: Int = 25, text: String): String {
+    val textList = ArrayList<String>()
+    textList.add(text.replace("\n", " "))
+    spaceDeleter(textList)
+    wordTransfer(width, textList)
     when (alignment) {
-        Alignment.JUSTIFY -> alignToWidth(width, text)
-        Alignment.CENTER -> centerAlignment(width, text)
-        Alignment.LEFT -> leftAlignment(width, text)
-        Alignment.RIGHT -> rightAlignment(width, text)
+        Alignment.JUSTIFY -> alignToWidth(width, textList)
+        Alignment.CENTER -> centerAlignment(width, textList)
+        Alignment.LEFT -> leftAlignment(width, textList)
+        Alignment.RIGHT -> rightAlignment(width, textList)
         //else -> return
     }
+    return textList.joinToString("\n")
 }
 
-private fun toOneLine(text: ArrayList<String>) {
-    for (i in text.size - 1 downTo 1) {
-        text[i - 1] = text[i - 1] + " " + text[i]
-        text.removeAt(text.lastIndex)
-    }
-}
-
-private fun spaceDeleter(text: ArrayList<String>) {
+private fun spaceDeleter(text: MutableList<String>) {
 // removing last spaces
     if (text[0][0] == ' ') {
         var i = 1
@@ -28,7 +23,6 @@ private fun spaceDeleter(text: ArrayList<String>) {
             i++
         text[0] = text[0].substring(i)
     }
-//////////////
 
 // removing first spaces
     if (text[0][text[0].length - 1] == ' ') {
@@ -37,7 +31,6 @@ private fun spaceDeleter(text: ArrayList<String>) {
             i--
         text[0] = text[0].substring(0, i + 1)
     }
-///////////////
 
 // removing duplicate spaces
     var i = 0
@@ -50,7 +43,6 @@ private fun spaceDeleter(text: ArrayList<String>) {
         }
         i++
     }
-///////////////
 
 // removing spaces before special symbols
     i = 1
@@ -62,7 +54,7 @@ private fun spaceDeleter(text: ArrayList<String>) {
     }
 }
 
-private fun wordTransfer(width: Int, text: ArrayList<String>) {
+private fun wordTransfer(width: Int, text: MutableList<String>) {
     var i = 0
     while (i < text.size) {
         while (text[i].length > width) {
@@ -72,39 +64,22 @@ private fun wordTransfer(width: Int, text: ArrayList<String>) {
             if (text[i].lastIndexOf(' ') != -1) {
                 // transfer one word if there is space
                 text[i + 1] = text[i].substringAfterLast(' ') +
-                        if (text[i + 1].isNotEmpty()) {
-                            " "
-                        } else {
-                            ""
-                        } + text[i + 1]
+                        if (text[i + 1].isNotEmpty()) " " else "" + text[i + 1]
                 text[i] = text[i].substringBeforeLast(' ')
             } else {
+                val transferredSymbols = if (text[i].length - width == 1 && width != 1 &&
+                    text[i].last() in arrayOf('!', ',', '.', ':', '-', ';', '?')
+                ) 1 else 0
                 // transfer few symbols if there isn't space
-                text[i + 1] = text[i].substring(
-                    width -
-                            if (text[i].length - width == 1 && text[i].last() in arrayOf(
-                                    '!',
-                                    ',',
-                                    '.',
-                                    ':',
-                                    '-',
-                                    ';',
-                                    '?'
-                                )
-                            ) {
-                                1
-                            } else {
-                                0
-                            }
-                ) + " " + text[i + 1]
-                text[i] = text[i].substring(0, width)
+                text[i + 1] = text[i].substring(width - transferredSymbols) + " " + text[i + 1]
+                text[i] = text[i].substring(0, width - transferredSymbols)
             }
         }
         i++
     }
 }
 
-private fun alignToWidth(width: Int, text: ArrayList<String>) {
+private fun alignToWidth(width: Int, text: MutableList<String>) {
     for (i in 0 until text.size) {
         if (text[i].length < width) {
             // needSpaces is count of spaces needed for add to line
@@ -135,17 +110,17 @@ private fun alignToWidth(width: Int, text: ArrayList<String>) {
     }
 }
 
-private fun leftAlignment(width: Int, text: ArrayList<String>) {
+private fun leftAlignment(width: Int, text: MutableList<String>) {
     for (i in 0 until text.size)
         text[i] = text[i] + " ".repeat(width - text[i].length)
 }
 
-private fun rightAlignment(width: Int, text: ArrayList<String>) {
+private fun rightAlignment(width: Int, text: MutableList<String>) {
     for (i in 0 until text.size)
         text[i] = " ".repeat(width - text[i].length) + text[i]
 }
 
-private fun centerAlignment(width: Int, text: ArrayList<String>) {
+private fun centerAlignment(width: Int, text: MutableList<String>) {
     for (i in 0 until text.size)
         text[i] = " ".repeat((width - text[i].length) / 2) + text[i] +
                 " ".repeat((width - text[i].length) - (width - text[i].length) / 2)
