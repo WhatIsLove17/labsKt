bea0951b1e7421224d29fe43a9c1bca26a00b10a
WhatIsLove <WhatIsLoveff@gmail.com>
2021-12-19 20:12:58 +0300

    naming was fixed

--- src/main/java/lab5/program/main.kt
+++ src/main/java/lab5/program/main.kt
@@ -9,20 +9,20 @@ val logger: Logger = LogManager.getLogger("Main function")
 
 fun main() {
     try {
-        val puk = LibraryServiceImpl()
+        val library = LibraryServiceImpl()
         val user1 = User("Ivan Ivanov")
         val user2 = User("Petr Petrov")
         val user3 = User("Alexandr Alexandrov")
 
-        puk.registerUser(user1)
-        puk.registerUser(user2)
-        puk.registerUser(user3)
+        library.registerUser(user1)
+        library.registerUser(user2)
+        library.registerUser(user3)
 
-        println(puk.getAllBooks())
+        println(library.getAllBooks())
 
-        puk.addBook(Book("Avengers", Year.parse("2012"), Author("Stan Lee"), Genre.Adventures))
-        puk.addBook(Book("Sherlock Holmes", Year.parse("1880"), Author("Conan Doyle"), Genre.Detectives))
-        puk.addBook(
+        library.addBook(Book("Avengers", Year.parse("2012"), Author("Stan Lee"), Genre.Adventures))
+        library.addBook(Book("Sherlock Holmes", Year.parse("1880"), Author("Conan Doyle"), Genre.Detectives))
+        library.addBook(
             Book(
                 "7 skills of highly effective people",
                 Year.parse("1989"),
@@ -30,25 +30,25 @@ fun main() {
                 Genre.BusinessLiterature
             )
         )
-        puk.addBook(Book("Omagad", Year.parse("2005"), Author("MDA MDA"), Genre.Dramaturgy))
+        library.addBook(Book("Omagad", Year.parse("2005"), Author("MDA MDA"), Genre.Dramaturgy))
 
 
-        println(puk.getAllAvailableBooks())
+        println(library.getAllAvailableBooks())
 
-        puk.takeBook(user1, puk.getAllBooks()[0])
-        //puk.takeBook(User("Petr Petrov"), puk.getAllBooks()[0])
-        puk.takeBook(user1, puk.getAllAvailableBooks()[0])
-        puk.takeBook(user1, puk.getAllAvailableBooks()[0])
+        library.takeBook(user1, library.getAllBooks()[0])
+        //library.takeBook(User("Petr Petrov"), library.getAllBooks()[0])
+        library.takeBook(user1, library.getAllAvailableBooks()[0])
+        library.takeBook(user1, library.getAllAvailableBooks()[0])
 
-        println(puk.getAllAvailableBooks())
-        puk.returnBook(puk.findBooks("Aven")[0])
-        puk.returnBook(puk.findBooks("Omag")[0])
-        println(puk.getAllAvailableBooks())
+        println(library.getAllAvailableBooks())
+        library.returnBook(library.findBooks("Aven")[0])
+        library.returnBook(library.findBooks("Omag")[0])
+        println(library.getAllAvailableBooks())
 
-        puk.takeBook(user2, puk.getAllAvailableBooks()[0])
-        puk.takeBook(user2, puk.getAllAvailableBooks()[0])
-        puk.takeBook(user2, puk.getAllAvailableBooks()[0])
-        println(puk.getAllAvailableBooks())
+        library.takeBook(user2, library.getAllAvailableBooks()[0])
+        library.takeBook(user2, library.getAllAvailableBooks()[0])
+        library.takeBook(user2, library.getAllAvailableBooks()[0])
+        println(library.getAllAvailableBooks())
 
     } catch (e: Exception) {
         logger.warn("exception has happen", e)
